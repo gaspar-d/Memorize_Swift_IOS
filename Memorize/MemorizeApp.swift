ea7240966b0de5242fb3372d407258b20b00194f
Diogo Gaspar <ghostlabz@gmail.com>
2021-01-29 19:43:14 -0300

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Diogo Gaspar on 29/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
